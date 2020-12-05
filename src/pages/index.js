523c0f01d7d885581bbaa3fb661e3a1e8a729181
Samuel ROZE <samuel.roze@gmail.com>
2020-12-05 11:56:37 +0000

    Use Tailwind's style components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
+import tw from "twin.macro"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const Button = tw.button`
+  bg-blue-500 hover:bg-blue-800 text-white p-2 rounded
+`
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +19,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Button>Foo</Button>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
